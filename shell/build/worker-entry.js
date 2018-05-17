3c0cc88ef4b1f38dfe42d408510696455016410d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-17 00:58:05 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- shell/build/worker-entry.js
+++ shell/build/worker-entry.js
@@ -1773,6 +1773,12 @@ class TypeChecker {
     let resolvedRight = right.type.resolvedType();
     let [leftType, rightType] = __WEBPACK_IMPORTED_MODULE_0__type_js__["a" /* Type */].unwrapPair(resolvedLeft, resolvedRight);
 
+    // an unconstrained variable is compatible with a set.
+    if (leftType.isVariable && !(leftType.variable.canReadSubset) && !(leftType.variable.canWriteSuperset) && rightType.isSetView)
+      return true;
+    if (rightType.isVariable && !(rightType.variable.canReadSubset) && !(rightType.variable.canWriteSuperset) && leftType.isSetView)
+      return true;
+
     if (leftType.isVariable || rightType.isVariable) {
       // TODO: everything should use this, eventually. Need to implement the
       // right functionality in Shapes first, though.
