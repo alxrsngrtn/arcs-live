04bdeed26d67f5a73e0f6eaa5bcd70dbd5216c29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-24 18:51:33 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- build/runtime/test/akms-test.js
+++ build/runtime/test/akms-test.js
@@ -7,70 +7,58 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
-
-import {KeyManager} from '../keymgmt/manager.js';
-import {assert} from '../../platform/chai-web.js';
-import {crypto} from '../../platform/crypto-web.js';
-
-describe('arcs key management', function() {
-  // Avoid initialising non-POD variables globally, since they would be constructed even when
-  // these tests are not going to be executed (i.e. another test file uses 'only').
-  let IV;
-  before(() => {
-    IV = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-  });
-
-  it('KeyManager supports generating wrapped Storage keys from DeviceKeys, and rewrapping them', async () => {
-    const keyGenerator = KeyManager.getGenerator();
-    const deviceKey = await keyGenerator.generateDeviceKey();
-    const cloudKey = await keyGenerator.generateDeviceKey();
-
-    const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
-    assert.isNotNull(storageKey);
-
-    const rewrappedKey = await storageKey.rewrap(deviceKey.privateKey(), cloudKey.publicKey());
-    assert.isNotNull(rewrappedKey);
-
-    // Test session key
-    const sessionKey = await rewrappedKey.unwrap(cloudKey.privateKey());
-    const testBuffer = new Uint8Array([42, 23, 99]);
-    const encryptBuffer = await sessionKey.encrypt(testBuffer, IV);
-    const decryptBuffer = await sessionKey.decrypt(encryptBuffer, IV);
-    assert.deepEqual(testBuffer, new Uint8Array(decryptBuffer));
-  });
-
-  it('KeyStorage supports persisting DeviceKeys and wrapped Storage Keys', async () => {
-    const keyGenerator = KeyManager.getGenerator();
-    const deviceKey = await keyGenerator.generateDeviceKey();
-    const keyStorage = KeyManager.getStorage();
-
-    const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
-    assert.isNotNull(storageKey);
-
-    await keyStorage.write(await deviceKey.fingerprint(), deviceKey);
-    const foundKey = await keyStorage.find(await deviceKey.fingerprint());
-    assert(await deviceKey.fingerprint(), await foundKey.fingerprint());
-
-    await keyStorage.write(await storageKey.fingerprint(), storageKey);
-    const foundStorageKey = await keyStorage.find(await storageKey.fingerprint());
-    assert(await foundStorageKey.fingerprint(), await storageKey.fingerprint());
-
-    const sessionKey = await storageKey.unwrap(deviceKey.privateKey());
-    const foundSessionKey = await foundStorageKey.unwrap(foundKey.privateKey());
-    const testBuffer = new Uint8Array([42, 23, 99]);
-    const encryptBuffer = await sessionKey.encrypt(testBuffer, IV);
-    const decryptBuffer = await foundSessionKey.decrypt(encryptBuffer, IV);
-    assert.deepEqual(testBuffer, new Uint8Array(decryptBuffer));
-  });
-
-  it('supports importing GCP PEM keys and exporting a rewrapped key', async () => {
-    const keyGenerator = KeyManager.getGenerator();
-    const deviceKey = await keyGenerator.generateDeviceKey();
-
-    const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
-    assert.isNotNull(storageKey);
-
-    const pemKey = `
+import { KeyManager } from '../keymgmt/manager.js';
+import { assert } from '../../platform/chai-web.js';
+import { crypto } from '../../platform/crypto-web.js';
+describe('arcs key management', () => {
+    // do not remove, typescript removes imports that are unsed
+    const storedCrypto = crypto;
+    // Avoid initialising non-POD variables globally, since they would be constructed even when
+    // these tests are not going to be executed (i.e. another test file uses 'only').
+    let IV;
+    before(() => {
+        IV = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+    });
+    it('KeyManager supports generating wrapped Storage keys from DeviceKeys, and rewrapping them', async () => {
+        const keyGenerator = KeyManager.getGenerator();
+        const deviceKey = await keyGenerator.generateDeviceKey();
+        const cloudKey = await keyGenerator.generateDeviceKey();
+        const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
+        assert.isNotNull(storageKey);
+        const rewrappedKey = await storageKey.rewrap(deviceKey.privateKey(), cloudKey.publicKey());
+        assert.isNotNull(rewrappedKey);
+        // Test session key
+        const sessionKey = await rewrappedKey.unwrap(cloudKey.privateKey());
+        const testBuffer = new Uint8Array([42, 23, 99]);
+        const encryptBuffer = await sessionKey['encrypt'](testBuffer, IV);
+        const decryptBuffer = await sessionKey['decrypt'](encryptBuffer, IV);
+        assert.deepEqual(testBuffer, new Uint8Array(decryptBuffer));
+    });
+    it('KeyStorage supports persisting DeviceKeys and wrapped Storage Keys', async () => {
+        const keyGenerator = KeyManager.getGenerator();
+        const deviceKey = await keyGenerator.generateDeviceKey();
+        const keyStorage = KeyManager.getStorage();
+        const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
+        assert.isNotNull(storageKey);
+        await keyStorage.write(await deviceKey.fingerprint(), deviceKey);
+        const foundKey = await keyStorage.find(await deviceKey.fingerprint());
+        assert(await deviceKey.fingerprint(), await foundKey.fingerprint());
+        await keyStorage.write(await storageKey.fingerprint(), storageKey);
+        const foundStorageKey = await keyStorage.find(await storageKey.fingerprint());
+        assert(await foundStorageKey.fingerprint(), await storageKey.fingerprint());
+        const sessionKey = await storageKey.unwrap(deviceKey.privateKey());
+        const foundSessionKey = await foundStorageKey['unwrap'](foundKey['privateKey']());
+        const testBuffer = new Uint8Array([42, 23, 99]);
+        const encryptBuffer = await sessionKey['encrypt'](testBuffer, IV);
+        const decryptBuffer = await foundSessionKey['decrypt'](encryptBuffer, IV);
+        assert.deepEqual(testBuffer, new Uint8Array(decryptBuffer));
+    });
+    it('supports importing GCP PEM keys and exporting a rewrapped key', async () => {
+        const keyGenerator = KeyManager.getGenerator();
+        const deviceKey = await keyGenerator.generateDeviceKey();
+        const storageKey = await KeyManager.getGenerator().generateWrappedStorageKey(deviceKey);
+        assert.isNotNull(storageKey);
+        const pemKey = `
 -----BEGIN CERTIFICATE-----
 MIIDpTCCAo2gAwIBAgIJAKHWfy0ksyvTMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
 BAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1Nb3VudGFpbiBW
@@ -94,14 +82,12 @@ psqMmQMry+xLiego9EZPkVmO/Nk6yI2d7OlVft6XCXmBmkgaaYPlWdy/8aq0koTt
 mxbV98vjuW6lMTn7t+DZN95f6IJn9AOnhw==
 -----END CERTIFICATE-----
 `;
-    const gcpPublicKey = await keyGenerator.importKey(pemKey);
-
-    const rewrappedKey = await storageKey.rewrap(deviceKey.privateKey(), gcpPublicKey);
-    assert.isNotNull(rewrappedKey);
-
-    const exportedRewrappedKey = rewrappedKey.export();
-    assert.isNotNull(exportedRewrappedKey);
-
-    // TODO: add a docker container on GCP that we can test this against
-  });
+        const gcpPublicKey = await keyGenerator.importKey(pemKey);
+        const rewrappedKey = await storageKey.rewrap(deviceKey.privateKey(), gcpPublicKey);
+        assert.isNotNull(rewrappedKey);
+        const exportedRewrappedKey = rewrappedKey.export();
+        assert.isNotNull(exportedRewrappedKey);
+        // TODO: add a docker container on GCP that we can test this against
+    });
 });
+//# sourceMappingURL=akms-test.js.map
