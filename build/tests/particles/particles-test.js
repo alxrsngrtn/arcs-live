df136b1cfbd46b59dc471d7fb03e0c0d62cc005a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 05:06:10 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- build/tests/particles/particles-test.js
+++ build/tests/particles/particles-test.js
@@ -2,15 +2,18 @@ import { fs } from '../../platform/fs-web.js';
 import { Manifest } from '../../runtime/manifest.js';
 import glob from 'glob';
 import { Loader } from '../../runtime/loader.js';
+import { assert } from '../../platform/chai-web.js';
 /** Tests that all .schema, .recipe(s) and .manifest files in the particles folder compile successfully. */
 describe('Particle definitions', () => {
     const loader = new Loader();
     const filenames = glob.sync('particles/**/*.{manifest,schema,recipe,recipes}');
     filenames
         .forEach(filename => {
-        const contents = fs.readFileSync(filename, 'utf8');
         it(`parses successfully: ${filename}`, async () => {
-            await Manifest.parse(contents, { fileName: filename, loader });
+            const manifest = await Manifest.load(filename, loader);
+            for (const particle of manifest.particles) {
+                assert.isTrue(fs.existsSync(particle.implFile), `${particle.implFile} not found`);
+            }
         });
     });
 });
