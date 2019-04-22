be7e9f19fb5f81def640e1beecf3d4f77eac1c00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-22 23:44:48 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- dist/index.es.js
+++ dist/index.es.js
@@ -28344,19 +28344,22 @@ class RamSlotComposer extends SlotComposer {
 }
 
 const version = '0_6_0';
-const volatile = 'volatile';
+const volatile = 'volatile://';
 const firebase = `firebase://arcs-storage.firebaseio.com/AIzaSyBme42moeI-2k8WgXh-6YK_wYyjEXo4Oz8/${version}`;
 const pouchdb = `pouchdb://local/arcs`;
 
 const Const = {
   version,
-  defaultUserId: 'user',
-  defaultFirebaseStorageKey: firebase,
-  defaultPouchdbStorageKey: pouchdb,
-  defaultStorageKey: pouchdb, //firebase,
-  defaultPlannerStorageKey: volatile,
-  defaultManifest: `https://$particles/canonical.manifest`,
-  launcherSuffix: `-launcher`,
+  DEFAULT: {
+    userId: 'user',
+    firebaseStorageKey: firebase,
+    pouchdbStorageKey: pouchdb,
+    volatileStorageKey: volatile,
+    storageKey: pouchdb, //firebase,
+    plannerStorageKey: 'volatile',
+    manifest: `https://$particles/canonical.manifest`,
+    launcherSuffix: `-launcher`,
+  },
   LOCALSTORAGE: {
     user: `${version}-user`,
     storage: `${version}-storage`,
@@ -29097,9 +29100,9 @@ class PlannerShellInterface {
 }
 
 // These are sample users for testing.
-PlannerShellInterface.DEFAULT_USER_ID = Const.defaultUserId;
-PlannerShellInterface.DEFAULT_STORAGE = Const.defaultStorageKey;
-PlannerShellInterface.DEFAULT_MANIFEST = Const.defaultManifest;
+PlannerShellInterface.DEFAULT_USER_ID = Const.DEFAULT.userId;
+PlannerShellInterface.DEFAULT_STORAGE = Const.DEFAULT.storageKey;
+PlannerShellInterface.DEFAULT_MANIFEST = Const.DEFAULT.manifest;
 
 const visualizeContext = context => {
   console.log('== context ===========================');
