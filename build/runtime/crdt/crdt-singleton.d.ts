67815691fe596961b9771c71e898353afd268711
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-15 01:17:31 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- build/runtime/crdt/crdt-singleton.d.ts
+++ build/runtime/crdt/crdt-singleton.d.ts
@@ -7,4 +7,42 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
+import { CRDTChange, CRDTModel, CRDTTypeRecord, VersionMap } from './crdt';
+declare type RawSingleton<T> = T;
+declare type SingletonData<T> = {
+    values: Map<T, VersionMap>;
+    version: VersionMap;
+};
+export declare enum SingletonOpTypes {
+    Set = 0,
+    Clear = 1
+}
+export declare type SingletonOperation<T> = {
+    type: SingletonOpTypes.Clear;
+    actor: string;
+    clock: VersionMap;
+} | {
+    type: SingletonOpTypes.Set;
+    value: T;
+    actor: string;
+    clock: VersionMap;
+};
+interface CRDTSingletonTypeRecord<T> extends CRDTTypeRecord {
+    data: SingletonData<T>;
+    operation: SingletonOperation<T>;
+    consumerType: RawSingleton<T>;
+}
+declare type SingletonChange<T> = CRDTChange<CRDTSingletonTypeRecord<T>>;
+declare type SingletonModel<T> = CRDTModel<CRDTSingletonTypeRecord<T>>;
+export declare class CRDTSingleton<T> implements SingletonModel<T> {
+    private collection;
+    merge(other: SingletonData<T>): {
+        modelChange: SingletonChange<T>;
+        otherChange: SingletonChange<T>;
+    };
+    applyOperation(op: SingletonOperation<T>): boolean;
+    getData(): SingletonData<T>;
+    getParticleView(): RawSingleton<T> | null;
+    private clear;
+}
 export {};
--- build/runtime/crdt/crdt-singleton.js
+++ build/runtime/crdt/crdt-singleton.js
@@ -7,4 +7,72 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
+import { ChangeType } from './crdt';
+import { CollectionOpTypes, CRDTCollection } from './crdt-collection';
+export var SingletonOpTypes;
+(function (SingletonOpTypes) {
+    SingletonOpTypes[SingletonOpTypes["Set"] = 0] = "Set";
+    SingletonOpTypes[SingletonOpTypes["Clear"] = 1] = "Clear";
+})(SingletonOpTypes || (SingletonOpTypes = {}));
+export class CRDTSingleton {
+    constructor() {
+        this.collection = new CRDTCollection();
+    }
+    merge(other) {
+        this.collection.merge(other);
+        // We cannot pass through the collection ops, so always return the updated model.
+        const change = {
+            changeType: ChangeType.Model,
+            modelPostChange: this.collection.getData()
+        };
+        return { modelChange: change, otherChange: change };
+    }
+    applyOperation(op) {
+        if (op.type === SingletonOpTypes.Clear) {
+            return this.clear(op.actor, op.clock);
+        }
+        if (op.type === SingletonOpTypes.Set) {
+            // Remove does not require an increment, but the caller of this method will have incremented
+            // its version, so we hack a version with t-1 for this actor.
+            const removeClock = (new Map(op.clock)).set(op.actor, op.clock.get(op.actor) - 1);
+            if (!this.clear(op.actor, removeClock)) {
+                return false;
+            }
+            const addOp = {
+                type: CollectionOpTypes.Add,
+                added: op.value,
+                actor: op.actor,
+                clock: op.clock,
+            };
+            if (!this.collection.applyOperation(addOp)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    getData() {
+        return this.collection.getData();
+    }
+    getParticleView() {
+        // Return any value.
+        return [...this.collection.getParticleView()].sort()[0] || null;
+    }
+    clear(actor, clock) {
+        // Clear all existing values if our clock allows it.
+        for (const value of this.collection.getData().values.keys()) {
+            const removeOp = {
+                type: CollectionOpTypes.Remove,
+                removed: value,
+                actor,
+                clock,
+            };
+            // If any value fails to remove, we haven't cleared the value and we fail the whole op.
+            //if (!this.collection.applyOperation(removeOp)) {
+            //   return false;
+            // }
+            this.collection.applyOperation(removeOp);
+        }
+        return true;
+    }
+}
 //# sourceMappingURL=crdt-singleton.js.map
