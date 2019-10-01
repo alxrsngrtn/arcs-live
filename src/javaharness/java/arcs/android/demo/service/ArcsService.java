7f96aa4469b6ba44d7bf4cc934d4ffb78f78c39d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-01 00:34:22 +0000

    Deploy PolymerLabs/arcs-live to github.com/PolymerLabs/arcs-live.git:master

--- src/javaharness/java/arcs/android/demo/service/ArcsService.java
+++ src/javaharness/java/arcs/android/demo/service/ArcsService.java
@@ -1,6 +1,6 @@
 package arcs.android.demo.service;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
@@ -14,9 +14,10 @@ import javax.inject.Inject;
 import arcs.android.api.IArcsService;
 import arcs.android.api.IRemoteOutputCallback;
 import arcs.android.api.IRemotePecCallback;
+import arcs.api.ArcData;
+import arcs.api.Arcs;
 import arcs.api.HarnessController;
 import arcs.api.PecPortManager;
-import arcs.api.PortableJson;
 import arcs.api.PortableJsonParser;
 import arcs.api.RemotePecPort;
 import arcs.api.ShellApiBasedArcsEnvironment;
@@ -26,19 +27,26 @@ import arcs.api.UiBroker;
  * ArcsService wraps Arcs runtime. Other Android activities/services are expected to connect to
  * ArcsService to communicate with Arcs.
  */
-public class ArcsService extends Service {
+// TODO: this is generic Arcs service class, move outside demo.
+public class ArcsService extends IntentService {
+  public static final String INTENT_REFERENCE_ID_FIELD = "intent_reference_id";
 
   private static final String TAG = "Arcs";
 
   private WebView arcsWebView;
   private boolean arcsReady;
 
+  @Inject Arcs arcs;
   @Inject HarnessController harnessController;
   @Inject ShellApiBasedArcsEnvironment shellEnvironment;
   @Inject PecPortManager pecPortManager;
   @Inject PortableJsonParser jsonParser;
   @Inject UiBroker uiBroker;
   @Inject NotificationRenderer notificationRenderer;
+
+  public ArcsService() {
+    super(ArcsService.class.getSimpleName());
+  }
 
   @Override
   public void onCreate() {
@@ -63,6 +71,20 @@ public class ArcsService extends Service {
     harnessController.init();
 
     uiBroker.registerRenderer("notification", notificationRenderer);
+  }
+
+  @Override
+  public void onDestroy() {
+    Log.d(TAG, "onDestroy()");
+    super.onDestroy();
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    super.onStartCommand(intent, flags, startId);
+    Log.d(TAG, "onStartCommand()");
+
+    return START_STICKY;
   }
 
   @Override
@@ -94,23 +116,18 @@ public class ArcsService extends Service {
                 },
                 jsonParser);
         pecPortManager.addRemotePecPort(pecId, remotePecPort);
-        // TODO: Use startArc method instead - should be factored out of DeviceClient.
-        PortableJson request =
-            jsonParser
-                .emptyObject()
-                .put("message", "runArc")
-                .put("arcId", arcId)
-                .put("recipe", recipe);
-        if (pecId != null) {
-          request.put("pecId", pecId);
-        }
-        if (providedSlotId != null) {
-          request.put("providedSlotId", providedSlotId);
-        }
-        if (particleId != null) {
-          request.put("particleId", particleId).put("particleName", particleName);
-        }
-        runWhenReady(() -> shellEnvironment.sendMessageToArcs(jsonParser.stringify(request)));
+
+        runWhenReady(
+            () ->
+                arcs.runArc(
+                    new ArcData.Builder()
+                        .setArcId(arcId)
+                        .setPecId(pecId)
+                        .setRecipe(recipe)
+                        .setParticleId(particleId)
+                        .setParticleName(particleName)
+                        .setProvidedSlotId(providedSlotId)
+                        .build()));
       }
 
       @Override
@@ -137,6 +154,19 @@ public class ArcsService extends Service {
             });
       }
     };
+  }
+
+  @Override
+  public boolean onUnbind(Intent intent) {
+    Log.d(TAG, "onUnbind()");
+    return super.onUnbind(intent);
+  }
+
+  @Override
+  protected void onHandleIntent(Intent intent) {
+    String referenceId = intent.getStringExtra(INTENT_REFERENCE_ID_FIELD);
+    Log.d(TAG, "Received referenceId " + referenceId);
+    // TODO: pass this ID to Arcs as part an input event.
   }
 
   private void runWhenReady(Runnable runnable) {
